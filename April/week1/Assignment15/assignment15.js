22a1a752fef450f800619268c743c6e4fafd1282
vimal <vimalsharma0753@gmail.com>
2022-04-10 06:56:26 -0700

    added files

--- April/week1/Assignment15/assignment15.js
+++ April/week1/Assignment15/assignment15.js
@@ -1,1 +1,10 @@
- 
+document.getElementById("img1").onmouseover = function() {mouseOver()};
+document.getElementById("img1").onmouseout = function() {mouseOut()};
+
+function mouseOver() {
+  document.getElementById("img1").style.height="400px" .width="500px";
+}
+
+function mouseOut() {
+  document.getElementById("img1").style.height="300px" .width="400px";
+}
