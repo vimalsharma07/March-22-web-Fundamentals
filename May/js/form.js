470d9b551d21f55ebd7932a65934aa4bb038c46f
Vimal Sharma <99383911+vimalsharma07@users.noreply.github.com>
2023-01-03 12:55:14 +0530

    Add files via upload

---
+++ May/js/form.js
@@ -0,0 +1,22 @@
+
+    function form(){
+        const name= document.getElementById("name").value
+        const email= document.getElementById("email").value
+        const subject= document.getElementById("subject").value
+        const message= document.getElementById("message").value
+        const number= document.getElementById("tel").value
+
+
+        alert("hihjihi");
+    
+ 
+
+    //     if( name=="" || email=="" || subject=="" || number=="" ||degree=="" ){
+    // alert("Please fill the all fields");
+    //     }else{
+    //         alert("form saves ")
+    //     }
+
+
+
+     }
